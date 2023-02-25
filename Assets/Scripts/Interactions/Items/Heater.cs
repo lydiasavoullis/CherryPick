2ae72cc68446c9a018d119816ad10001b29f642b
Lydia <lydiasavoullis@gmail.com>
2023-02-25 14:22:27 +0000

    fix heater issue

--- Assets/Scripts/Interactions/Items/Heater.cs
+++ Assets/Scripts/Interactions/Items/Heater.cs
@@ -3,19 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class Heater : MonoBehaviour, IBeginDragHandler
+public class Heater : MonoBehaviour, IBeginDragHandler, IEndDragHandler
 {
     public void OnBeginDrag(PointerEventData eventData)
     {
-        GameObject heater = DragHandler.itemBeingDragged;
-        if (heater.tag == "heater")
-        {
-            //Debug.Log("turn off heaters");
-            heater.transform.GetChild(0).gameObject.SetActive(false);
-        }
-        
+        //Debug.Log("turn off heaters");
+        gameObject.transform.GetChild(0).gameObject.SetActive(false);
 
     }
 
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        if ( gameObject.transform.parent.tag == "heaterContainer")
+        {
+            //Debug.Log("turn on heaters");
+            gameObject.transform.GetChild(0).gameObject.SetActive(true);
+        }
+    }
 }
 
